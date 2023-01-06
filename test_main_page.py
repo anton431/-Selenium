14586bb0502540288475b2657029a5b4ca132a6f
anton <anton.dontsovgs@mail.ru>
2023-01-06 12:21:52 +0300

    init

---
+++ test_main_page.py
@@ -0,0 +1,5 @@
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(by = 'css selector', value = "#login_link")
+    login_link.click()
