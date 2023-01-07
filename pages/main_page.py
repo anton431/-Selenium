af96340f1ff6777490443b21a86e872dd00a77eb
anton <anton.dontsovgs@mail.ru>
2023-01-07 13:57:48 +0300

    checking an element on a page

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(by = 'css selector', value = "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present('css selector', "#login_link"), "Login link is not presented"
