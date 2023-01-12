996944b41a49714ff8dd7fd4589d44d49b0c1653
anton <anton.dontsovgs@mail.ru>
2023-01-12 15:38:00 +0300

    add LoginPage

--- pages/locators.py
+++ pages/locators.py
@@ -1,2 +1,6 @@
 class MainPageLocators():
     LOGIN_LINK = ('css selector','#login_link')
+
+class LoginPageLocators():
+    login_form = ('css selector', "#login_form")
+    register_form =('css selector', "#register_form")
