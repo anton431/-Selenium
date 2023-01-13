0365f064056ecec950d023644e76d657e2d15873
anton <anton.dontsovgs@mail.ru>
2023-01-13 17:19:51 +0300

    update text

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -3,7 +3,7 @@ from .locators import ProductPageLocators
 
 class BasketPage(BasePage):
     def sould_be_empty_basket(self):
-        assert not self.is_not_element_present(*ProductPageLocators.basket_massage), "Basket message is presented, but should not be"
+        assert not self.is_not_element_present(*ProductPageLocators.basket_massage), "Basket is not empty"
 
     def sould_be_empty_basket_message(self):
-        assert not self.is_disappeared(*ProductPageLocators.basket_massage), "Basket message is presented, but should disappeared"
+        assert not self.is_disappeared(*ProductPageLocators.basket_massage), "Basket message is not presented"
